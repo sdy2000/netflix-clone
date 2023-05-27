d412d5cc2787885c059fd0c9202fdb45abb2e379
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-05-27 17:52:34 +0430

    Create AccountingButton component and update global.css

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
+// / // / // / // Shared // / // / // / //
 export { default as Header } from "./shared/Header";
 export { default as BackgroundImage } from "./shared/BackgroundImage";
+
+// / // / // / // Button // / // / // / //
+export { default as AccountingButton } from "./button/AccountingButton";
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,18 +1,13 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import AccountingButton from "../button/AccountingButton";
 
 const Header = (props) => {
-  const navigate = useNavigate();
   return (
     <div className="container flex justify-between items-center w-full">
       <Link to="/" className="hover:scale-105 duration-300">
         <img className="h-12 md:h-20" src="/assets/logo.png" alt="Logo" />
       </Link>
-      <button
-        className="bg-red-600 text-white font-bold text-lg px-4 py-1 rounded-lg hover:bg-red-800 hover:scale-105 duration-300"
-        onClick={() => navigate(props.login ? "/login" : "/signup")}
-      >
-        {props.login ? "Log In" : "Sign In"}
-      </button>
+      <AccountingButton props={props} />
     </div>
   );
 };
