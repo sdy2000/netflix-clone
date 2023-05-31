733f601795e1f09dc59786ac8c1153fde34f3895
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-05-31 18:05:06 +0430

    Create constants for TMDB Key and add stor for project

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  movies: [],
+  genresLoaded: false,
+  genres: [],
+};
+
+const NetflixSlice = createSlice({
+  name: "Netflix",
+  initialState,
+  extraReducers: (builder) => {},
+});
+export const store = configureStore({
+  reducer: {
+    netflix: NetflixSlice.reducer,
+  },
+});
---
+++ src/utils/constants/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "3a884123b17759b90c60e554cf4d7ba5";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
